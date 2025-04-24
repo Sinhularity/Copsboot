efa0a60371ee32743f1e95715c82bae51363a346
Sinhularity <zs22017021@estudiantes.uv.mx>
2025-04-24 13:55:30 -0600

    Part seven : dependencies added

---
+++ src/test/java/mx/uv/coatza/S22017021/copsboot/infrastructure/SpringProfiles.java
@@ -0,0 +1,6 @@
+package mx.uv.coatza.S22017021.copsboot.infrastructure;
+
+public class SpringProfiles {
+    public static final String REPOSITORY_TEST = "repository-test";
+    public static final String INTEGRATION_TEST = "integration-test";
+}
